0f80182204fd6cd51c8523f3fb0ddd2d1653889c
Marco <marcogilbertorm@gmail.com>
2026-04-20 20:52:54 -0600

    Fix podspec deployment target to 16.2 and use local ison asset

--- pomodoro-app/targets/widget/expo-target.config.js
+++ pomodoro-app/targets/widget/expo-target.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
   module.exports = config => ({                                                                                      
     type: "widget",                                                                                                  
-    icon: 'https://github.com/expo.png',                                                                             
+    icon: '../../assets/images/icon.png',                                                                             
     // Minimum iOS 16.2 required for ActivityKit Live Activities                                   
     deploymentTarget: "16.2",                                                          
     frameworks: ['ActivityKit'],                                                                                     
